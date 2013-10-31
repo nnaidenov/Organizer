136e2434fbe6b018e1aaf46e42fae359e7c93999
nnaidenov <n.naidenov92@gmail.com>
2013-10-31 13:29:29 +0200

    Add 2 activities and layouts
    Add Login and Register activity with their layouts.

---
+++ src/me/naidenov/organizer/LoginActivity.java
@@ -0,0 +1,19 @@
+package me.naidenov.organizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
--- src/me/naidenov/organizer/MainActivity.java
+++ src/me/naidenov/organizer/MainActivity.java
@@ -2,14 +2,27 @@ package me.naidenov.organizer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button buttonRegister;
+	private Button buttonLogin;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		buttonRegister = (Button) findViewById(R.id.button_register_view);
+		buttonRegister.setOnClickListener(this);
+
+		buttonLogin = (Button) findViewById(R.id.button_login_view);
+		buttonLogin.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +30,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.button_login_view) {
+			Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+			startActivity(intent);
+		} else if (v.getId() == R.id.button_register_view) {
+			Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+			startActivity(intent);
+		}
 	}
 
 }
