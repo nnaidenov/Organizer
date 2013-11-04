e241dfc28b91e470724774d6805a3a9cc58c3000
nnaidenov <n.naidenov92@gmail.com>
2013-11-04 02:15:39 +0200

    Add functionality
    Save photo to sd card

--- src/me/naidenov/organizer/CreateNoteActivity.java
+++ src/me/naidenov/organizer/CreateNoteActivity.java
@@ -1,21 +1,42 @@
 package me.naidenov.organizer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.xml.sax.InputSource;
+
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+@TargetApi(Build.VERSION_CODES.FROYO)
 public class CreateNoteActivity extends Activity implements OnClickListener {
 	private static final int CAMERA_CODE = 0;
+	private static final int LOAD_IMAGES_CODE = 1;
 
 	private Button camera_button;
+	private Button photo_button;
 	private ImageView image_imageView;
+	private String filePath;
+	private String filename;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -26,6 +47,8 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 		boolean hasCamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
 
 		camera_button = (Button) findViewById(R.id.button_note_camera);
+		photo_button = (Button) findViewById(R.id.button_note_photo);
+		photo_button.setOnClickListener(this);
 		if (hasCamera) {
 			camera_button.setOnClickListener(this);
 		} else {
@@ -47,6 +70,11 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 			Intent intent = new Intent(
 					android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 			startActivityForResult(intent, CAMERA_CODE);
+		} else if (v.getId() == R.id.button_note_photo) {
+			Intent intent = new Intent(
+					Intent.ACTION_PICK,
+					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			startActivityForResult(intent, LOAD_IMAGES_CODE);
 		}
 	}
 
@@ -58,6 +86,62 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 			if (extras != null) {
 				Bitmap image = (Bitmap) extras.get("data");
 				image_imageView.setImageBitmap(image);
+
+				String iconsStoragePath = Environment
+						.getExternalStorageDirectory() + "/myAppDir/myImages/";
+				String name = "dsds";
+
+				File file = new File(iconsStoragePath, name);
+
+				// Convert bitmap to byte array
+				Bitmap bitmap = image;
+				ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				bitmap.compress(CompressFormat.PNG, 0 /* ignored for PNG */, bos);
+				byte[] bitmapdata = bos.toByteArray();
+
+				// write the bytes in file
+				FileOutputStream fos = null;
+				try {
+					fos = new FileOutputStream(file);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					fos.write(bitmapdata);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					fos.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				Toast.makeText(CreateNoteActivity.this,
+						"Your Path:", 2000).show();
+				
+				Uri selectedImage = data.getData();
+				String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+				Cursor cursor = getContentResolver().query(selectedImage,
+						filePathColumn, null, null, null);
+				cursor.moveToFirst();
+
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+				// file path of captured image
+				filePath = cursor.getString(columnIndex);
+				// file path of captured image
+				File f = new File(filePath);
+				filename = f.getName();
+
+				Toast.makeText(CreateNoteActivity.this,
+						"Your Path:" + filePath, 2000).show();
+				Toast.makeText(CreateNoteActivity.this,
+						"Your Filename:" + filename, 2000).show();
+				cursor.close();
 			}
 		}
 	}
