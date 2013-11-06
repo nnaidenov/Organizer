b73b68c0b19f4475cb1619c1bf7fececc2b5bfd0
nnaidenov <n.naidenov92@gmail.com>
2013-11-06 23:55:54 +0200

    Upload image to cloud

--- src/me/naidenov/organizer/CreateNoteActivity.java
+++ src/me/naidenov/organizer/CreateNoteActivity.java
@@ -1,5 +1,6 @@
 package me.naidenov.organizer;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,8 +8,15 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xml.sax.InputSource;
+
+import com.cloudinary.Cloudinary;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -19,10 +27,12 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -33,13 +43,14 @@ import android.widget.Toast;
 @TargetApi(Build.VERSION_CODES.FROYO)
 public class CreateNoteActivity extends Activity implements OnClickListener {
 	private static final int CAMERA_CODE = 0;
+	private static final int VIDEO_CODE = 2;
 	private static final int LOAD_IMAGES_CODE = 1;
 
 	private Button camera_button;
+	private Button video_button;
 	private Button photo_button;
 	private ImageView image_imageView;
-	private String filePath;
-	private String filename;
+	private String mCapturedImageURI;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -50,10 +61,13 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 		boolean hasCamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
 
 		camera_button = (Button) findViewById(R.id.button_note_camera);
+		video_button = (Button) findViewById(R.id.button_note_video);
+
 		photo_button = (Button) findViewById(R.id.button_note_photo);
 		photo_button.setOnClickListener(this);
 		if (hasCamera) {
 			camera_button.setOnClickListener(this);
+			video_button.setOnClickListener(this);
 		} else {
 			camera_button.setVisibility(View.GONE);
 		}
@@ -72,12 +86,16 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 		if (v.getId() == R.id.button_note_camera) {
 			Intent intent = new Intent(
 					android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
 			startActivityForResult(intent, CAMERA_CODE);
 		} else if (v.getId() == R.id.button_note_photo) {
 			Intent intent = new Intent(
 					Intent.ACTION_PICK,
 					android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 			startActivityForResult(intent, LOAD_IMAGES_CODE);
+		} else if (v.getId() == R.id.button_note_video) {
+			Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+			startActivityForResult(takeVideoIntent, VIDEO_CODE);
 		}
 	}
 
@@ -90,18 +108,32 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 				Bitmap image = (Bitmap) extras.get("data");
 				image_imageView.setImageBitmap(image);
 
+				File folder = new File(
+						Environment.getExternalStorageDirectory()
+								+ "/myAppDir/myImages/");
+				if (!folder.exists()) {
+					if (folder.mkdirs())
+						Toast.makeText(this, "New Folder Created",
+								Toast.LENGTH_SHORT).show();
+				}
+
 				String iconsStoragePath = Environment
 						.getExternalStorageDirectory() + "/myAppDir/myImages/";
-				String name = "dsds";
+				long dafa = System.currentTimeMillis() / 1000;
+
+				String name = "IMG_" + String.valueOf(dafa) + ".png";
 
 				File file = new File(iconsStoragePath, name);
-
 				// Convert bitmap to byte array
 				Bitmap bitmap = image;
 				ByteArrayOutputStream bos = new ByteArrayOutputStream();
 				bitmap.compress(CompressFormat.PNG, 0 /* ignored for PNG */, bos);
 				byte[] bitmapdata = bos.toByteArray();
 
+				InputStream is = new ByteArrayInputStream(bitmapdata);
+				
+				uploadPicture aa = new uploadPicture();
+				aa.execute(is);
 				// write the bytes in file
 				FileOutputStream fos = null;
 				try {
@@ -122,15 +154,20 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
 			}
 		} else if (requestCode == LOAD_IMAGES_CODE) {
-			String iconsStoragePath = Environment.getExternalStorageDirectory()
-					+ "/myAppDir/myImages/";
-			String name = "dsdsa1";
 
-			File file2 = new File(iconsStoragePath, name);
+			File folder = new File(Environment.getExternalStorageDirectory()
+					+ "/myAppDir/myImages/");
+			if (!folder.exists()) {
+				if (folder.mkdirs())
+					Toast.makeText(this, "New Folder Created",
+							Toast.LENGTH_SHORT).show();
+			}
 
+			if (data == null) {
+				return;
+			}
 			Uri selectedImage = data.getData();
 			String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
@@ -140,13 +177,25 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 
 			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
 			// file path of captured image
-			filePath = cursor.getString(columnIndex);
+			String filePath = cursor.getString(columnIndex);
+			File f = new File(filePath);
+			String filename = f.getName();
 			cursor.close();
 
 			Bitmap bitmap = BitmapFactory.decodeFile(filePath);
 			ByteArrayOutputStream bos = new ByteArrayOutputStream();
 			bitmap.compress(CompressFormat.PNG, 0 /* ignored for PNG */, bos);
 			byte[] bitmapdata = bos.toByteArray();
+
+			String iconsStoragePath = Environment.getExternalStorageDirectory()
+					+ "/myAppDir/myImages/";
+
+			File file2 = new File(iconsStoragePath, filename);
+			
+			InputStream is = new ByteArrayInputStream(bitmapdata);
+			
+			uploadPicture aa = new uploadPicture();
+			aa.execute(is);
 
 			// write the bytes in file
 			FileOutputStream fos = null;
@@ -168,6 +217,40 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		} else if (requestCode == VIDEO_CODE)
+
+			Toast.makeText(this, "Record", Toast.LENGTH_LONG).show();
+	}
+	
+private class uploadPicture extends AsyncTask<InputStream, Void, Void>{
+
+	@Override
+	protected Void doInBackground(InputStream... arg0) {
+
+		InputStream is = arg0[0];
+		
+		Map config = new HashMap();
+		config.put("cloud_name", "djlwcsyiz");
+		config.put("api_key", "781383948985498");
+		config.put("api_secret", "Vh5BQmeTxvSKvTGTg-wRDYKqPz4");
+		Cloudinary cloudinary = new Cloudinary(config);
+		cloudinary.url().generate("saaa");
+		
+		try {
+
+		JSONObject dsd = cloudinary.uploader().upload(is, Cloudinary.emptyMap());
+		String loudScreaming = dsd.getString("url");
+		Log.d("My", loudScreaming);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		return null;
+	}
+		
+		
 	}
 }
