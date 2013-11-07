2d1810e66c451e61639f645e8c3eec93431b14a8
nnaidenov <n.naidenov92@gmail.com>
2013-11-07 15:30:31 +0200

    Fully functionality note

--- src/me/naidenov/organizer/CreateNoteActivity.java
+++ src/me/naidenov/organizer/CreateNoteActivity.java
@@ -1,5 +1,7 @@
 package me.naidenov.organizer;
 
+
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -8,18 +10,27 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.xml.sax.InputSource;
 
 import com.cloudinary.Cloudinary;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -37,20 +48,32 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-@TargetApi(Build.VERSION_CODES.FROYO)
 public class CreateNoteActivity extends Activity implements OnClickListener {
 	private static final int CAMERA_CODE = 0;
 	private static final int VIDEO_CODE = 2;
 	private static final int LOAD_IMAGES_CODE = 1;
+
+	private byte[] photo;
+	private byte[] image;
+	private List<String> resourseUrls = new ArrayList<String>();
+
+	private EditText editorText_title;
+	private EditText editorText_description;
 
 	private Button camera_button;
 	private Button video_button;
 	private Button photo_button;
 	private ImageView image_imageView;
 	private String mCapturedImageURI;
+
+	private Button save_button;
+	private String newNoteModel;
+	
+	private int readyToSend = 0;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -59,6 +82,9 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 
 		PackageManager pm = this.getPackageManager();
 		boolean hasCamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+
+		editorText_title = (EditText) findViewById(R.id.editText_note_title);
+		editorText_description = (EditText) findViewById(R.id.editText_note_description);
 
 		camera_button = (Button) findViewById(R.id.button_note_camera);
 		video_button = (Button) findViewById(R.id.button_note_video);
@@ -73,6 +99,9 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 		}
 
 		image_imageView = (ImageView) findViewById(R.id.imageView);
+
+		save_button = (Button) findViewById(R.id.button_note_save);
+		save_button.setOnClickListener(this);
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -96,6 +125,51 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 		} else if (v.getId() == R.id.button_note_video) {
 			Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
 			startActivityForResult(takeVideoIntent, VIDEO_CODE);
+		} else if (v.getId() == R.id.button_note_save) {
+
+			
+
+			if (image != null) {
+				InputStream is = new ByteArrayInputStream(photo);
+
+				uploadPicture aa = new uploadPicture();
+				aa.execute(is);
+			}
+
+			if (photo != null) {
+				InputStream is = new ByteArrayInputStream(photo);
+
+				uploadPicture aa = new uploadPicture();
+				aa.execute(is);
+			}
+			
+			if (readyToSend == 0) {
+				String title = String.valueOf(editorText_title.getText());
+				String description = String.valueOf(editorText_description
+						.getText());
+
+				List<String> images = resourseUrls;
+
+				JSONObject object = new JSONObject();
+				try {
+					object.put("Title", title);
+					object.put("Text", description);
+					object.put("Images", images);
+
+				} catch (Exception ex) {
+				}
+
+				newNoteModel = object.toString();
+				
+				CreateNote noter = new CreateNote();
+				noter.execute(new String[] {
+						"http://mobileorganizer.apphb.com/api/notes/create",
+						newNoteModel });
+			}
+		} else if (v.getId() == R.id.button_note_back) {
+			Intent goToHomePageIntent = new Intent(new Intent(
+					CreateNoteActivity.this, HomeActivity.class));
+			startActivity(goToHomePageIntent);
 		}
 	}
 
@@ -128,12 +202,8 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 				Bitmap bitmap = image;
 				ByteArrayOutputStream bos = new ByteArrayOutputStream();
 				bitmap.compress(CompressFormat.PNG, 0 /* ignored for PNG */, bos);
-				byte[] bitmapdata = bos.toByteArray();
+				photo = bos.toByteArray();
 
-				InputStream is = new ByteArrayInputStream(bitmapdata);
-				
-				uploadPicture aa = new uploadPicture();
-				aa.execute(is);
 				// write the bytes in file
 				FileOutputStream fos = null;
 				try {
@@ -143,7 +213,7 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 					e.printStackTrace();
 				}
 				try {
-					fos.write(bitmapdata);
+					fos.write(photo);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -154,6 +224,7 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				readyToSend++;
 			}
 		} else if (requestCode == LOAD_IMAGES_CODE) {
 
@@ -185,17 +256,12 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 			Bitmap bitmap = BitmapFactory.decodeFile(filePath);
 			ByteArrayOutputStream bos = new ByteArrayOutputStream();
 			bitmap.compress(CompressFormat.PNG, 0 /* ignored for PNG */, bos);
-			byte[] bitmapdata = bos.toByteArray();
+			image = bos.toByteArray();
 
 			String iconsStoragePath = Environment.getExternalStorageDirectory()
 					+ "/myAppDir/myImages/";
 
 			File file2 = new File(iconsStoragePath, filename);
-			
-			InputStream is = new ByteArrayInputStream(bitmapdata);
-			
-			uploadPicture aa = new uploadPicture();
-			aa.execute(is);
 
 			// write the bytes in file
 			FileOutputStream fos = null;
@@ -206,7 +272,7 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 				e.printStackTrace();
 			}
 			try {
-				fos.write(bitmapdata);
+				fos.write(image);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -217,40 +283,146 @@ public class CreateNoteActivity extends Activity implements OnClickListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
+			readyToSend++;
 		} else if (requestCode == VIDEO_CODE)
 
 			Toast.makeText(this, "Record", Toast.LENGTH_LONG).show();
 	}
-	
-private class uploadPicture extends AsyncTask<InputStream, Void, Void>{
 
-	@Override
-	protected Void doInBackground(InputStream... arg0) {
+	private class uploadPicture extends AsyncTask<InputStream, Void, String> {
 
-		InputStream is = arg0[0];
-		
-		Map config = new HashMap();
-		config.put("cloud_name", "djlwcsyiz");
-		config.put("api_key", "781383948985498");
-		config.put("api_secret", "Vh5BQmeTxvSKvTGTg-wRDYKqPz4");
-		Cloudinary cloudinary = new Cloudinary(config);
-		cloudinary.url().generate("saaa");
-		
-		try {
+		private ProgressDialog progress;
 
-		JSONObject dsd = cloudinary.uploader().upload(is, Cloudinary.emptyMap());
-		String loudScreaming = dsd.getString("url");
-		Log.d("My", loudScreaming);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		@Override
+		protected String doInBackground(InputStream... arg0) {
+
+			InputStream is = arg0[0];
+
+			Map config = new HashMap();
+			config.put("cloud_name", "djlwcsyiz");
+			config.put("api_key", "781383948985498");
+			config.put("api_secret", "Vh5BQmeTxvSKvTGTg-wRDYKqPz4");
+			Cloudinary cloudinary = new Cloudinary(config);
+
+			String loudScreaming = null;
+			try {
+
+				JSONObject dsd = cloudinary.uploader().upload(is,
+						Cloudinary.emptyMap());
+				loudScreaming = dsd.getString("url");
+				Log.d("My", loudScreaming);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return loudScreaming;
 		}
-		return null;
+		
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			progress = ProgressDialog.show(CreateNoteActivity.this, "Wait",
+					"Please wait");
+			progress.show();
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			progress.dismiss();
+			Toast.makeText(CreateNoteActivity.this, result, Toast.LENGTH_LONG).show();
+			resourseUrls.add(result);
+			readyToSend--;
+			
+			if (readyToSend == 0) {
+				String title = String.valueOf(editorText_title.getText());
+				String description = String.valueOf(editorText_description
+						.getText());
+
+				List<String> images = resourseUrls;
+				JSONArray jsArray = new JSONArray(images);
+
+				JSONObject object = new JSONObject();
+				try {
+					object.put("Title", title);
+					object.put("Text", description);
+					object.put("Images", jsArray);
+
+				} catch (Exception ex) {
+				}
+
+				newNoteModel = object.toString();
+				
+				CreateNote noter = new CreateNote();
+				noter.execute(new String[] {
+						"http://mobileorganizer.apphb.com/api/notes/create",
+						newNoteModel });
+			}
+		}
 	}
-		
-		
+
+	private class CreateNote extends AsyncTask<String, Void, Void> {
+
+		private Dialog progress;
+
+		@Override
+		protected Void doInBackground(String... params) {
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost(params[0]);
+
+			try {
+				FileInputStream os = null;
+				try {
+					os = openFileInput("sessionKey");
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				BufferedReader r = new BufferedReader(new InputStreamReader(os));
+				StringBuilder total = new StringBuilder();
+				String line;
+				try {
+					while ((line = r.readLine()) != null) {
+						total.append(line);
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				post.setEntity(new StringEntity(params[1], "UTF8"));
+				post.setHeader("Content-type", "application/json");
+				post.setHeader("X-sessionKey", total.toString());
+				client.execute(post);
+			} catch (Exception e) {
+
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			// UserDb userDb =
+			// ((OrganizerApplication)getApplication()).getUserDb();
+			//
+			// userDb.addUser(email, authCode, sessionKey);
+			progress.dismiss();
+			Toast.makeText(CreateNoteActivity.this, "Created",
+					Toast.LENGTH_LONG).show();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			progress = ProgressDialog.show(CreateNoteActivity.this, "Wait",
+					"Please wait");
+			progress.show();
+		}
+
 	}
 }
